875d4f46cfa0e4402ca1430b2e347c46782e8c3c
jamiezhangming <jamiezhangming@didiglobal.com>
2021-11-12 11:20:54 +0800

    add pointcut

--- src/main/java/com/jamie/demo/DemoApplication.java
+++ src/main/java/com/jamie/demo/DemoApplication.java
@@ -2,8 +2,9 @@ package com.jamie.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class}, scanBasePackages = { "com.jamie.study" })
 public class DemoApplication {
 
     public static void main(String[] args) {
