f37d793abe06284840b9b9323ae052a7389148e8
jamiezhangming <jamiezhangming@didiglobal.com>
2021-11-21 12:56:56 +0800

    fix

---
+++ src/main/java/com/jamie/study/annotation/EnableRedisLock.java
@@ -0,0 +1,24 @@
+package com.jamie.study.annotation;
+
+import java.lang.annotation.*;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @PackageName: com.jamie.study.annotation
+ * @ClassName: EnableRedisLock
+ * @Description:
+ * @Author: jamiezhangming
+ * @Date: 2021/11/20 5:01 下午
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@Documented
+public @interface EnableRedisLock {
+    String lockKey();
+
+    long expireTime() default 5;
+
+    TimeUnit timeUnit() default  TimeUnit.SECONDS;
+
+    int retryTime() default 10;
+}
