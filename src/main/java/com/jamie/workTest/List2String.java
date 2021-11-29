af2b22623ba99056992973bbe2a84deb2ababba1
jamiezhangming <jamiezhangming@didiglobal.com>
2021-11-29 21:02:49 +0800

    add test

---
+++ src/main/java/com/jamie/workTest/List2String.java
@@ -0,0 +1,24 @@
+package com.jamie.workTest;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @PackageName: com.jamie.workTest
+ * @ClassName: List2String
+ * @Description:
+ * @Author: jamiezhangming
+ * @Date: 2021/11/29 6:10 下午
+ */
+public class List2String {
+
+    public static void main(String[] args) {
+        List<Integer> cityList = new ArrayList<>();
+        cityList.add(1);
+        cityList.add(3);
+        cityList.add(4);
+        cityList.add(5);
+        System.out.println(cityList.stream().map(String::valueOf).collect(Collectors.joining(",")));
+    }
+}
