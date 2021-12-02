87e42b6ef515df7ab4c009462569d67c3544ee4c
jamiezhangming <jamiezhangming@didiglobal.com>
2021-12-02 19:14:45 +0800

    fix

--- src/main/java/com/jamie/jvmstudy/ExecutorDemo.java
+++ src/main/java/com/jamie/jvmstudy/ExecutorDemo.java
@@ -19,6 +19,16 @@ public class ExecutorDemo {
 
     /**
      * newScheduledThreadPool 主要用来延迟执行任务，或者定时执行任务。功能和Timer类似，但是ScheduleThreadPoolExecutor更强大、更灵活一些。Timer后台是单个线程，而ScheduleThreadPoolExecutor可以在创建的时候指定多个线程。
+     *
+     *
+     *
+     * 在《阿里巴巴java开发手册》中指出了线程资源必须通过线程池提供，不允许在应用中自行显示的创建线程，这样一方面是线程的创建更加规范，可以合理控制开辟线程的数量；另一方面线程的细节管理交给线程池处理，优化了资源的开销。而线程池不允许使用Executors去创建，而要通过ThreadPoolExecutor方式，这一方面是由于jdk中Executor框架虽然提供了如newFixedThreadPool()、newSingleThreadExecutor()、newCachedThreadPool()等创建线程池的方法，但都有其局限性，不够灵活；另外由于前面几种方法内部也是通过ThreadPoolExecutor方式实现，使用ThreadPoolExecutor有助于大家明确线程池的运行规则，创建符合自己的业务场景需要的线程池，避免资源耗尽的风险。
+     *
+     *
+     *
+     *
+     *
+     *
      * @param args
      * @throws ExecutionException
      * @throws InterruptedException
@@ -46,6 +56,7 @@ public class ExecutorDemo {
                 TimeUnit.SECONDS.sleep(1);
                 int increment = atomicInteger.getAndIncrement();
                 System.out.println(Thread.currentThread().getName() + "b执行测试:" + increment);
+                System.out.println(10/0);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -53,6 +64,9 @@ public class ExecutorDemo {
 
         scheduledExecutor.scheduleAtFixedRate(runnable, 1,1, TimeUnit.SECONDS);
         scheduledExecutor.scheduleAtFixedRate(runnable2, 1,1, TimeUnit.SECONDS);
+
+
+
         //scheduledExecutor.schedule(runnable2, 2, TimeUnit.SECONDS);
 
         /*scheduledExecutor.schedule(() ->{
@@ -78,6 +92,8 @@ public class ExecutorDemo {
 
         }, 10, TimeUnit.SECONDS);*/
 
-
+        TimeUnit.SECONDS.sleep(10);
+        scheduledExecutor.shutdown();
+        System.out.println(scheduledExecutor.isShutdown());
     }
 }
