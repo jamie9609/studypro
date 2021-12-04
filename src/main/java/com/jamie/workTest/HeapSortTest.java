1e4ef4ecffdfbf85f9fe5f7c6db91cc3b691dd86
jamiezhangming <jamiezhangming@didiglobal.com>
2021-12-04 14:18:29 +0800

    heap sort

--- src/main/java/com/jamie/workTest/HeapSortTest.java
+++ src/main/java/com/jamie/workTest/HeapSortTest.java
@@ -1,11 +1,5 @@
 package com.jamie.workTest;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
-
 
 /**
  * @PackageName: com.jamie.workTest
@@ -16,49 +10,72 @@ import java.util.stream.Collectors;
  */
 public class HeapSortTest {
 
-    public static List<Integer> HeapSortDemo(List<Integer> source) {
-        int n = source.size();
+    /**
+     * 堆排序，它最好和最坏的情况时间复杂度都是O(nlogn)，空间复杂度O(1)。
+     * @param source
+     * @return
+     */
 
-        List<Integer> ans = new ArrayList<>();
-        ans.add(Integer.MAX_VALUE);
-        for (Integer item : source) {
-            ans.add(item);
-            swimAuto(ans);
+    public static int[] HeapSortDemo(int[] source) {
+        if (source.length == 1){
+            return source;
+        }
+        int n = source.length;
+
+        //从第一个非叶子节点开始调整，先构建大顶堆
+        for (int i = parentKey(n); i >= 0; i --) {
+            dropAuto(source, i, n);
+        }
+        //将堆顶元素移动到最末尾，重排。
+        for (int j = n - 1; j > 0; j --) {
+            exchange(source, 0, j);
+            dropAuto(source, 0, j);
         }
 
-        return ans;
+        return source;
     }
 
     /**
-     * 上浮
+     * 下沉，从上到下，依次遍历。
      * @param ans
      */
-    public static void swimAuto(List<Integer> ans) {
-        int sKey = ans.size() - 1;
-        if (sKey == 1) {
+    public static void dropAuto(int[] ans, int index, int length) {
+        if (index < 0) {
             return;
         }
-        while (sKey > 1) {
-            if (parentKey(sKey) > 0 && ans.get(parentKey(sKey)) < ans.get(sKey) ) {
-                exchange(ans, parentKey(sKey), sKey);
-            }
-            sKey = parentKey(sKey);
+        // 默认当前节点（父节点）是最大值。
+        int largestIndex = index;
+        if (leftChildKey(index) <= length - 1 && ans[largestIndex] < ans[leftChildKey(index)] ) {
+            largestIndex = leftChildKey(index);
+        }
+
+        if (rightChildKey(index) <= length - 1 && ans[largestIndex] < ans[rightChildKey(index)]) {
+            largestIndex = rightChildKey(index);
+
+        }
+
+        if (largestIndex != index) {
+            exchange(ans, index, largestIndex);
+            dropAuto(ans, largestIndex, length);
         }
     }
 
     public static int leftChildKey(int root) {
-        return root * 2;
-    }
-    public static int leftRightKey(int root) {
         return root * 2 + 1;
     }
-    public static int parentKey(int root) {
-        return root / 2;
+    public static int rightChildKey(int root) {
+        return root * 2 + 2;
     }
-    public static <E> void exchange (List<E> list, int index1, int index2 ) {
-        E tmp = list.get(index1);
-        list.set(index1, list.get(index2));
-        list.set(index2, tmp);
+    public static int parentKey(int root) {
+        if (root == 0) {
+            return -1;
+        }
+        return root / 2 - 1 / 2;
+    }
+    public static void exchange (int[] list, int index1, int index2 ) {
+        int tmp = list[index1];
+        list[index1] = list[index2];
+        list[index2] = tmp;
     }
 
     /**
@@ -66,13 +83,15 @@ public class HeapSortTest {
      * @param args
      */
     public static void main(String[] args) {
-        Integer[] case1 = new Integer[]{1,2,23,4,5,13,1, 10};
-        List<Integer> ints = Arrays.asList(case1).stream().collect(Collectors.toList());
+        int n = 8;
 
-        List<Integer> integers = HeapSortDemo(ints);
+        int[] case1 = new int[]{12,12,33333,1,2,23,4,5,13,1,10};
+        //List<Integer> ints = Arrays.asList(case1).stream().collect(Collectors.toList());
 
-        for (int i = 1; i < integers.size(); i ++) {
-            System.out.println(integers.get(i) + ",");
+        int[] integers = HeapSortDemo(case1);
+
+        for (int i = 0; i < integers.length; i ++) {
+            System.out.println(integers[i] + ",");
         }
     }
 }
