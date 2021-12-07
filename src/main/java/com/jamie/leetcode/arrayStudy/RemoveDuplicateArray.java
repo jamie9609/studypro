6317c5cef9ae181f83d2384c292b35a208ee02e2
jamiezhangming <jamiezhangming@didiglobal.com>
2021-12-07 16:43:05 +0800

    fix

--- src/main/java/com/jamie/leetcode/arrayStudy/RemoveDuplicateArray.java
+++ src/main/java/com/jamie/leetcode/arrayStudy/RemoveDuplicateArray.java
@@ -49,6 +49,91 @@ public class RemoveDuplicateArray {
         return sb.reverse().toString();
     }
 
+    /**
+     * 给你一个有序数组 nums ，请你 原地 删除重复出现的元素，使每个元素 只出现一次 ，返回删除后数组的新长度。
+     * @param nums
+     * @return
+     */
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int slow = 0, fast = 0;
+        while (fast < nums.length) {
+            if (nums[fast] != nums[slow]) {
+                slow++;
+                // 维护 nums[0..slow] 无重复
+                nums[slow] = nums[fast];
+            }
+            fast++;
+        }
+        // 数组长度为索引 + 1
+        return slow + 1;
+    }
+
+    public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+
+    public ListNode deleteDuplicates2(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode slow = head, fast = head;
+        while (fast != null) {
+            if (fast.val != slow.val) {
+                slow.next = fast;
+                slow = slow.next;
+            }
+            fast = fast.next;
+        }
+        slow.next = null;
+        return head;
+    }
+
+
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode fast = head, slow = head;
+        while (fast != null) {
+            if (fast.val != slow.val) {
+                slow.next = fast;
+                slow = slow.next;
+            }
+            fast = fast.next;
+        }
+
+        slow.next = null;
+        return head;
+    }
+
+    public void moveZeroes(int[] nums) {
+
+        int p = removeElement(nums, 0);
+        for (; p < nums.length; p++) {
+            nums[p] = 0;
+        }
+    }
+
+    public int removeElement(int[] nums, int val) {
+        int fast = 0, slow = 0;
+        while (fast < nums.length) {
+            if (nums[fast] != val) {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+            fast++;
+        }
+        return slow;
+    }
+
     public static void main(String[] args) {
         System.out.println( removeDuplicateLetters("bbcaac"));
     }
