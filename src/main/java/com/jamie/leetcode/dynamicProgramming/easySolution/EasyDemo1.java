89151168d01dc39cca77a9be9a53a5d58bfc3a72
jamiezhangming <jamiezhangming@didiglobal.com>
2021-12-08 09:40:20 +0800

    add

--- src/main/java/com/jamie/leetcode/dynamicProgramming/easySolution/EasyDemo1.java
+++ src/main/java/com/jamie/leetcode/dynamicProgramming/easySolution/EasyDemo1.java
@@ -67,9 +67,43 @@ public class EasyDemo1 {
         return dp[amount];
     }
 
+    /**
+     * 给你一个整数数组 nums ，找到其中最长严格递增子序列的长度。
+     * 子序列是由数组派生而来的序列，删除（或不删除）数组中的元素而不改变其余元素的顺序。例如，[3,6,2,7] 是数组 [0,3,1,6,2,2,7] 的子序列。
+     * @param nums
+     * @return
+     */
+    public static int lengthOfLIS(int[] nums) {
+        if(nums.length == 0) {
+            return 0;
+        }
+        /**
+         * dp[i] 表示以nums[i]结尾的最长子序列长度。
+         */
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1);
+
+        for (int i = 0; i < nums.length; i ++) {
+            for (int j = 0; j < i; j ++) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        int res = 0;
+        for (int i = 0; i < dp.length; i++) {
+            res = Math.max(res, dp[i]);
+        }
+        return res;
+    }
+
+
+
+
     public static void main(String[] args) {
-        int[] case1 = {2};
-        System.out.println(coinChange(case1, 4));
+        int[] case1 = {10,9,2,5,3,7,101,18};
+        //System.out.println(coinChange(case1, 4));
+        System.out.println(lengthOfLIS(case1));
     }
 
 }
