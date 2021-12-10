021ad103890607e9ed635cc85b86dcca6a39425c
jamiezhangming <jamiezhangming@didiglobal.com>
2021-12-10 09:36:23 +0800

    add

--- src/main/java/com/jamie/leetcode/subsequenceStudy/DynamicProgram.java
+++ src/main/java/com/jamie/leetcode/subsequenceStudy/DynamicProgram.java
@@ -229,10 +229,40 @@ public class DynamicProgram {
         return totalSum - dp[n][m];
     }
 
+    /**
+     * 给你一个字符串 s ，找出其中最长的回文子序列，并返回该序列的长度。
+     *
+     * 子序列定义为：不改变剩余字符顺序的情况下，删除某些字符或者不删除任何字符形成的一个序列。
+     * @param s
+     * @return
+     */
+    public static int longestPalindromeSubseq(String s) {
+        int n = s.length();
+        if (n == 0) {
+            return 0;
+        }
+        //dp[i][j]为在i、j索引之间的字符串的最长回文子序列长度
+        int[][] dp = new int[n + 1][n + 1];
+        for (int i = 0; i < n; i ++) {
+            dp[i][i] = 1;
+        }
+
+        for (int i = n - 1; i >= 0; i --) {
+            for (int j = i + 1; j < n; j ++) {
+
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = dp[i + 1][j - 1] + 2;
+                } else {
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[0][n -1];
+    }
 
     public static void main(String[] args) {
-        String text1 = "abcde";
+        String text1 = "bbbab";
         String text2 = "ace";
-        System.out.println(longestCommonSubsequence(text1, text2));
+        System.out.println(longestPalindromeSubseq(text1));
     }
 }
