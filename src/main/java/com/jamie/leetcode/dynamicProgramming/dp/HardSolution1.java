9595822bd48599fcca579426eb85c6d4752f0535
jamiezhangming <jamiezhangming@didiglobal.com>
2021-12-21 11:35:07 +0800

    fix

--- src/main/java/com/jamie/leetcode/dynamicProgramming/dp/HardSolution1.java
+++ src/main/java/com/jamie/leetcode/dynamicProgramming/dp/HardSolution1.java
@@ -158,6 +158,28 @@ public class HardSolution1 {
         return dp[N];
     }
 
+
+
+
+    public static int minInsertions(String s) {
+        int n = s.length();
+        int[][] dp = new int[n][n];
+
+        for (int i = n - 2; i >= 0; i --) {
+            for (int j = i + 1; j < n; j ++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = dp[i + 1][j - 1];
+                } else {
+                    dp[i][j] = Math.min(dp[i + 1][j], dp[i][j - 1]) + 1;
+                }
+            }
+        }
+        return dp[0][n - 1];
+    }
+
+
+
+
     public static void main(String[] args) {
         int[] case2 = {3, 1, 5, 8};
         System.out.println(maxCoins(case2));
