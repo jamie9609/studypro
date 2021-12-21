3c5bdf9465182c277dc26999b5a807200e733ddc
jamiezhangming <jamiezhangming@didiglobal.com>
2021-12-21 17:34:46 +0800

    fix

--- src/main/java/com/jamie/leetcode/dynamicProgramming/dp/HardSolution1.java
+++ src/main/java/com/jamie/leetcode/dynamicProgramming/dp/HardSolution1.java
@@ -248,6 +248,43 @@ public class HardSolution1 {
         return count;
     }
 
+    /**
+     * 会议室
+     * @param meetings
+     * @return
+     */
+    public int minMeetingRooms(int[][] meetings) {
+        int n = meetings.length;
+        int[] begin = new int[n];
+        int[] end = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            begin[i] = meetings[i][0];
+            end[i] = meetings[i][1];
+        }
+
+        Arrays.sort(begin);
+        Arrays.sort(end);
+
+        int count = 0;
+
+        // 双指针技巧
+        int res = 0, i = 0, j = 0;
+        while ( i < n && j < n ) {
+            if (begin[i] < end[j]) {
+                count++;
+                i++;
+            } else {
+                count--;
+                j++;
+            }
+            res = Math.max(res, count);
+        }
+        return res ;
+    }
+
+
+
     public static void main(String[] args) {
         int[] case1 = {-2147483646,-2147483645};
         int[] case2 = {2147483646,2147483647};
