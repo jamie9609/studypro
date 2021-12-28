9e161629e49ea481c2264c17975dd4a221ae0945
jamiezhangming <jamiezhangming@didiglobal.com>
2021-12-28 17:25:43 +0800

    fix

--- src/main/java/com/jamie/leetcode/bitOperation/EasySolution.java
+++ src/main/java/com/jamie/leetcode/bitOperation/EasySolution.java
@@ -175,5 +175,44 @@ public class EasySolution {
         return (part1 * part2) % base;
     }
 
+    public int missingNumber(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int n = nums.length;
+        int tmp = (1 + n) * n /2;
+        int res = 0;
+        for (int i : nums) {
+            res += i;
+        }
+        return tmp - res;
+    }
+
+    /**
+     * 集合 s 包含从 1 到 n 的整数。不幸的是，因为数据错误，导致集合里面某一个数字复制了成了集合里面的另外一个数字的值，导致集合 丢失了一个数字 并且 有一个数字重复。
+     *
+     * @param nums
+     * @return
+     */
+    public int[] findErrorNums(int[] nums) {
+        int n = nums.length;
+        int dup = -1;
+        for (int i = 0; i < n; i++) {
+            // 现在的元素是从 1 开始的
+            int index = Math.abs(nums[i]) - 1;
+            if (nums[index] < 0)
+                dup = Math.abs(nums[i]);
+            else
+                nums[index] *= -1;
+        }
+
+        int missing = -1;
+        for (int i = 0; i < n; i++)
+            if (nums[i] > 0)
+                // 将索引转换成元素
+                missing = i + 1;
+
+        return new int[]{dup, missing};
+    }
 
 }
