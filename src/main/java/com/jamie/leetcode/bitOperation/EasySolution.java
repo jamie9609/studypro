54d1d6b5a35730f888b5b1b2304bab51e73d8e77
jamiezhangming <jamiezhangming@didiglobal.com>
2021-12-29 14:20:55 +0800

    fix

--- src/main/java/com/jamie/leetcode/bitOperation/EasySolution.java
+++ src/main/java/com/jamie/leetcode/bitOperation/EasySolution.java
@@ -275,4 +275,27 @@ public class EasySolution {
         }
         return res;
     }
+
+    /**
+     * 给你一个有序数组 nums ，请你 原地 删除重复出现的元素，使每个元素 只出现一次 ，返回删除后数组的新长度。
+     * 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+     * @param nums
+     * @return
+     */
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int slow = 0, fast = 0;
+        while (fast < nums.length) {
+            if (nums[fast] != nums[slow]) {
+                slow++;
+                // 维护 nums[0..slow] 无重复
+                nums[slow] = nums[fast];
+            }
+            fast++;
+        }
+        // 数组长度为索引 + 1
+        return slow + 1;
+    }
 }
